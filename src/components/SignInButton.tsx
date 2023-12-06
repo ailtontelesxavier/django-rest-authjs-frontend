b02464be194de438533f2fd10af694cb6e846595
ailton <ailton.xavier@fomento.to.gov.br>
2023-12-06 16:28:10 -0300

    opcao de deslogar

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -12,16 +12,23 @@ const SignInButton = () => {
     console.log(session);
     console.log(useSession());
     console.log(status === 'authenticated');
+    console.log(session?.access_token);
     if (status === 'authenticated') {
         //router.push("profile");
         //return;
       }
     return (
         <>
+        {session ? <span>Estou logado</span> : <span>nao logado</span>}
         <button type="button"
         onClick={() => signIn("credentials", credentials, {callbackUrl: "/interno"})}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:bg-blue-gray-400">
             Login
+        </button>
+        <button type="button"
+        onClick={() => signOut("credentials")}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:bg-blue-gray-400">
+            Sair
         </button>
         </>
     );
